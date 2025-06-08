3d2affa08a8ff55464a325bc0b72bf10adf4e60d
MazenYasser <mazenyasserx47@gmail.com>
2025-06-08 18:59:11 +0300

    Removed unimplemented action routes from view in main menu

--- cli_interface/Questionary/question_routing.py
+++ cli_interface/Questionary/question_routing.py
@@ -9,13 +9,13 @@ class ActionChoices(Enum):
     CHANGE_CONFIG = "Change configurations"
     EXIT = "Exit"
 
-action_choices = [choice.value for choice in ActionChoices]
-
 routes = {
     ActionChoices.UPLOAD.value: trigger_upload_sequence,
     ActionChoices.DOWNLOAD.value: trigger_download_sequence,
     ActionChoices.CHANGE_CONFIG.value: trigger_config_change_sequence,
     ActionChoices.EXIT.value: trigger_exit_sequence,
 }
+
+action_choices = [choice.value for choice in ActionChoices if choice.value in routes.keys()]
 
 
